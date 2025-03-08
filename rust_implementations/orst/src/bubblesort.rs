5e265d2ceb1a6cda769044ae7e8230fc46c7e00d
Trust-Worthy <jondeveloper0@gmail.com>
2025-03-08 06:17:54 -0500

    bubblesort is super simple. Very inefficient algorithm.

--- rust_implementations/orst/src/bubblesort.rs
+++ rust_implementations/orst/src/bubblesort.rs
@@ -10,6 +10,22 @@ impl Sorter for Bubblesort {
     fn sort<T>(slice: &mut [T]) where T: Ord {
         
         // TO-DO
+      
+        let mut swapped = true;
+        // Keep doing this until entire array is walked without having to set 
+        // swapped to true!
+        while swapped {
+            swapped = false;
+
+            for i in 0..slice.len() {
+                if slice[i] > slice[i + 1] {
+                    slice.swap(i, i+1);
+                    swapped = true;
+                }
+            }
+        }
+
+        
     }
 
 }
